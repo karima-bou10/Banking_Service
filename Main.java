431d715a570f03ca0e810443f3264c53be7c73a9
karima-bou10 <karima.bouachra1010@gmail.com>
2025-12-16 03:38:37 +0100

    Modify Main Class

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		Account account = new Account();
 		
-		account.deposit(3000);
         account.deposit(1000);
-        account.withdraw(500);
         account.deposit(2000);
         account.withdraw(500);
 
